3dc15f527f4020cac077e50581ba581c4c216bc6
Lucas Maciel <lalucasforte56@gmail.com>
2023-12-23 16:17:59 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var nome = document.querySelector('#name');
+var texto = nome.innerHTML;
+console.log(nome, texto);
+nome.addEventListener('mouseenter', () => {
+    nome.innerHTML = '@lucazf4';
+    nome.setAttribute('href', 'https://instagram.com/lucazf4');
+})
+nome.addEventListener('mouseleave', () => {
+    nome.innerHTML = 'Lucas Maciel';
+    nome.setAttribute('href', '#');
+
+})
